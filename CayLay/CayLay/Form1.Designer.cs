7373c13527610dd22b649fe26ba0db2260081fae
ququ600 <50228468+ququ600@users.noreply.github.com>
2020-10-04 21:04:24 +0800

    Add files via upload

---
+++ CayLay/CayLay/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace CayLay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtbLLength = new System.Windows.Forms.TextBox();
+            this.txtbRLength = new System.Windows.Forms.TextBox();
+            this.txtbLength = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtbLRect = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtbRRect = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(663, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(663, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 25);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtbLLength
+            // 
+            this.txtbLLength.Location = new System.Drawing.Point(663, 252);
+            this.txtbLLength.Name = "txtbLLength";
+            this.txtbLLength.Size = new System.Drawing.Size(75, 25);
+            this.txtbLLength.TabIndex = 3;
+            // 
+            // txtbRLength
+            // 
+            this.txtbRLength.Location = new System.Drawing.Point(663, 198);
+            this.txtbRLength.Name = "txtbRLength";
+            this.txtbRLength.Size = new System.Drawing.Size(75, 25);
+            this.txtbRLength.TabIndex = 4;
+            // 
+            // txtbLength
+            // 
+            this.txtbLength.Location = new System.Drawing.Point(663, 145);
+            this.txtbLength.Name = "txtbLength";
+            this.txtbLength.Size = new System.Drawing.Size(75, 25);
+            this.txtbLength.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(663, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "递归深度";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(663, 234);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "左分支长度比";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(663, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "右分支长度比";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(663, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "主干长度";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(663, 348);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "左分支角度";
+            // 
+            // txtbLRect
+            // 
+            this.txtbLRect.Location = new System.Drawing.Point(663, 366);
+            this.txtbLRect.Name = "txtbLRect";
+            this.txtbLRect.Size = new System.Drawing.Size(75, 25);
+            this.txtbLRect.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(663, 290);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "右分支角度";
+            // 
+            // txtbRRect
+            // 
+            this.txtbRRect.Location = new System.Drawing.Point(663, 308);
+            this.txtbRRect.Name = "txtbRRect";
+            this.txtbRRect.Size = new System.Drawing.Size(75, 25);
+            this.txtbRRect.TabIndex = 12;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(663, 418);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(663, 400);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 15);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "颜色";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 481);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtbRRect);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtbLRect);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbLength);
+            this.Controls.Add(this.txtbRLength);
+            this.Controls.Add(this.txtbLLength);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtbLLength;
+        private System.Windows.Forms.TextBox txtbRLength;
+        private System.Windows.Forms.TextBox txtbLength;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtbLRect;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtbRRect;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ CayLay/CayLay/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CayLay
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        public double th1 { get; set; }
+        public double th2 { get; set; }
+        public double per1 { get; set; }
+        public double per2 { get; set; }
+        public double Length { get; set; }
+        public double n { get; set; }
+        public Pen drawPen { get; set; }
+        void drawCayleyTree(int n,double x0,double y0,double leng,double th)
+        {
+           
+                if (n == 0)
+                {
+                    return;
+                }
+                double x1 = x0 + leng * Math.Cos(th);
+                double y1 = y0 + leng * Math.Sin(th);
+
+            drawLine(x0, y0, x1, y1);
+            drawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            drawCayleyTree(n - 1, x1, y1, per2 * leng, th - th2);
+        }
+        void drawLine(double x0,double y0, double x1, double y1)
+        {
+            graphics.DrawLine(Pens.Blue, (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            th1 = 30 * Math.PI / 180;
+            th2 = 20 * Math.PI / 180;
+            per1 = 0.6;
+            per2 = 0.7;
+            Length = 100;
+            n = 10;
+            txtbLength.DataBindings.Add("Text", this, "Length");
+            textBox1.DataBindings.Add("Text", this, "n");
+            txtbRLength.DataBindings.Add("Text", this, "per1");
+            txtbLLength.DataBindings.Add("Text", this, "per2");
+            txtbLRect.DataBindings.Add("Text", this, "th1");
+            txtbRRect.DataBindings.Add("Text", this, "th2");
+            Pen[] pens = { Pens.Red, Pens.Green, Pens.Yellow };
+            comboBox1.DataSource = pens;
+            comboBox1.DisplayMember = "Color";
+            comboBox1.DataBindings.Add("SelectedItem", this, "DrawPen");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            th1 = Convert.ToDouble(textBox1.Text);
+            if (graphics == null) graphics = this.CreateGraphics();
+            drawCayleyTree(10, 200, 310, 100, -Math.PI / 2);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
